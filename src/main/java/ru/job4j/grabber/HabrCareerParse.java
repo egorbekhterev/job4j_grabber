d07cdcbad2eadfe53bda61f586bd044a580ac030
Paranoj <e.behterev@yandex.ru>
2022-12-11 20:22:51 +0700

    2.1.1. Парсинг https://career.habr.com/vacancies/java_developer?page=1 [#285210 #369916]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -13,10 +13,11 @@ import java.util.Objects;
 public class HabrCareerParse {
     private static final String SOURCE_LINK = "https://career.habr.com";
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
+    private static final int PAGE_COUNTER = 5;
 
     public static void main(String[] args) throws IOException {
-        for (int i = 1; i < 6; i++) {
-            Connection connection = Jsoup.connect(String.format(PAGE_LINK, "%s", i));
+        for (int i = 1; i <= PAGE_COUNTER; i++) {
+            Connection connection = Jsoup.connect(String.format(PAGE_LINK.concat("%s"), i));
             Document document = connection.get();
             Elements rows = document.select(".vacancy-card__inner");
             rows.forEach(row -> {
