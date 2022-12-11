c106708d9b772fd779406e1c0076502259580710
Paranoj <e.behterev@yandex.ru>
2022-12-11 20:17:03 +0700

    2.1. Преобразование даты [#289476 #369636]

--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -4,9 +4,10 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class HabrCareerDateTimeParser implements DateTimeParser {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+
     @Override
     public LocalDateTime parse(String parse) {
-        DateTimeFormatter formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
-        return LocalDateTime.parse(parse, formatter);
+        return LocalDateTime.parse(parse, FORMATTER);
     }
 }
--- src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
+++ src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.*;
 class HabrCareerDateTimeParserTest {
 
     @Test
-    void checkParserFromIsoOffsetDateTimeToLocalDateTime() {
+    void whenParserFromIsoOffsetDateTimeToLocalDateTime() {
         String parse = "2022-12-09T22:27:17+03:00";
         LocalDateTime actual = new HabrCareerDateTimeParser().parse(parse);
         String expected = "2022-12-09T22:27:17";
